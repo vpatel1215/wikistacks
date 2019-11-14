9775932d1ff41e2052770fad6b114375bb235f69
Vishal Patel <vpatel1215@gmail.com>
2019-11-14 16:17:27 -0600

    finished add html to the add page

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { addPage } = require("../views");
+const { addPage, wikiPage } = require("../views");
 const { Page, User } = require("../models/");
 
 // /wiki
@@ -15,8 +15,8 @@ router.post("/", async (req, res, next) => {
       content: req.body.content,
       status: req.body.status
     });
-    console.log("PAGE LOOKS LIKE: ", page);
-    await res.redirect(`/${page.slug}`);
+    
+    await res.redirect(`/wiki/${page.slug}`);
   } catch (error) {
     next(error);
   }
@@ -26,8 +26,17 @@ router.get("/add", async (req, res) => {
   res.send(addPage());
 });
 
-router.get("/:slug", (req, res, next) => {
-  res.send(`hit dynamic route at ${req.params.slug}`);
+router.get("/:slug", async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug
+      }
+    });
+    console.log(wikiPage);
+    res.send(wikiPage(page));
+  } catch (error) { next(error) }
+
 });
 
 module.exports = router;
